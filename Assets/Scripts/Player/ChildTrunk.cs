ecac5ff5f627b8f1c4fbfd5903edd186155f88c9
M4A1CID <konata_lzumi@hotmail.com>
2016-08-24 14:29:18 +0800

    Added Trunk Character hinge
    Todo: Trunk to follow VR camera with rotation physics

--- Assets/Scripts/Player/ChildTrunk.cs
+++ Assets/Scripts/Player/ChildTrunk.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class ChildTrunk : MonoBehaviour {
 
+    
+    public GameObject OVRcamera;
+    public float RotationSpeed;
+
+    private Quaternion currentRotation;
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        //transform.position = OVRcamera.transform.position + v3Offset;
+        //currentRotation = transform.rotation;
+        //currentRotation.set
+        //transform.rotation = Quaternion.Slerp(transform.rotation, OVRcamera.transform.rotation, RotationSpeed * Time.deltaTime);
 	}
 }
