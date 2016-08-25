5f0caa67e264701d427ea3fc217a7f4519db4a8e
M4A1CID <konata_lzumi@hotmail.com>
2016-08-25 15:36:08 +0800

    Added in Texture painting
    Youtube Link:
    https://www.youtube.com/watch?v=WiR2zgX324c&list=PLit65otflU8_DXXm2Qc1FexuoyxX8O5p9&index=1

---
+++ Assets/Scripts/TexturePainting/PlaneProcess.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneProcess : MonoBehaviour {
+	public Texture2D MainTexture;
+	public Texture2D SpriteTexture;
+	private ComputeBitmap computeBitmap=new ComputeBitmap();
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.renderer.material.mainTexture = computeBitmap.BitmapsAddMix (MainTexture, SpriteTexture) as Texture;
+	}
+}
