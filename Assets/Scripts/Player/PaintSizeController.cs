5f99ae176c78ce47d3195796a1dfde82b24428e3
M4A1CID <konata_lzumi@hotmail.com>
2016-08-26 11:31:35 +0800

    Added a paint size changer to the scene

---
+++ Assets/Scripts/Player/PaintSizeController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaintSizeController : MonoBehaviour {
+
+    public GameObject SnoutGun;
+
+    public int NumberOfArrays;
+    public Texture2D[] SmallSplat;
+    public Texture2D[] NormalSplat;
+    public Texture2D[] BigSplat;
+    
+    // 0 for S, 1 for M, 2 for L
+    public int currentSize;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public Texture2D[] GetSwitchSize()
+    {
+        currentSize += 1;
+        // If switching causes the access of array to go out of scope
+        if (currentSize >= NumberOfArrays)
+        {
+            // Reset back the size
+            currentSize = 0;
+        }
+
+        // Do a switch statment based on current size
+        switch(currentSize)
+        {
+            case 0:
+                return SmallSplat;
+            case 1:
+                return NormalSplat;
+            case 2:
+                return BigSplat;
+            default:
+                return NormalSplat;
+
+        }
+    }
+
+
+
+
+}
--- Assets/Scripts/TexturePainting/RayEmitter.cs
+++ Assets/Scripts/TexturePainting/RayEmitter.cs
@@ -25,20 +25,29 @@ public class RayEmitter : MonoBehaviour {
         RaycastHit hitInfo;
         Vector3 directionalVector = snout.transform.position - Camera.main.transform.position; //The direction that the paint will spray in
         bool hit = Physics.Raycast(snout.transform.position, directionalVector, out hitInfo, Mathf.Infinity); //Raycast to determine has the paint hit anything?
-        Debug.DrawRay(snout.transform.position, directionalVector ); //Draw the ray in the scene for testing
+        Debug.DrawRay(snout.transform.position, directionalVector); //Draw the ray in the scene for testing
 
 
         if (Input.GetButtonDown("Fire1"))
         {
-            
 
-			//if (hit)
+
+            // If ray hits the canvas
             if (hitInfo.collider.gameObject.CompareTag("Canvas"))
+            {
                 RayXY = hitInfo.textureCoord;
-            int randomSplat = Random.Range(0, SpriteTexture.Length-1);
-			MainTexture=computeBitmap.BitmapsAddMix (MainTexture, SpriteTexture[randomSplat],SpriteColor,RayXY.x,RayXY.y);
-			PlaneObj.transform.GetComponent<Renderer>().material.mainTexture =MainTexture as Texture;
+                int randomSplat = Random.Range(0, SpriteTexture.Length - 1);
+                MainTexture = computeBitmap.BitmapsAddMix(MainTexture, SpriteTexture[randomSplat], SpriteColor, RayXY.x, RayXY.y);
+                PlaneObj.transform.GetComponent<Renderer>().material.mainTexture = MainTexture as Texture;
+            }
+            
+            // If ray hits Size changer
+            if (hitInfo.collider.gameObject.CompareTag("SizeChanger"))
+            {
+               SpriteTexture =  hitInfo.collider.gameObject.GetComponent<PaintSizeController>().GetSwitchSize();
+            }
 
-			}
-		}
+
+        }
+    }
 }
