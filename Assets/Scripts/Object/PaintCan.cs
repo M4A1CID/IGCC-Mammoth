b52be3c2a37c1d0ff3b6e0bc654d1d1ad7709d1e
M4A1CID <konata_lzumi@hotmail.com>
2016-08-29 14:15:39 +0800

    Added new studio scene

--- Assets/Scripts/Object/PaintCan.cs
+++ Assets/Scripts/Object/PaintCan.cs
@@ -4,11 +4,12 @@ using System.Collections;
 public class PaintCan : MonoBehaviour {
 
     public Color PaintColor;
-
+    public string ColorName;
 
 	// Use this for initialization
 	void Start () {
-	
+        // Convert to Uppercase
+        ColorName = ColorName.ToUpper();
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/Player/PaintMixer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaintMixer : MonoBehaviour {
+
+    public string[] ArrayOfColor;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public Color AddTwoColor(GameObject PaintBucket_1, GameObject PaintBucket_2)
+    {
+        // First Color Red
+        if (PaintBucket_1.GetComponent<PaintCan>().ColorName == "RED")
+        {
+            // Mix with Yellow
+            
+
+            // Mix with Blue
+        }
+
+        // First Color Yellow
+        else if (PaintBucket_1.GetComponent<PaintCan>().ColorName == "YELLOW")
+        {
+            // Mix with Yellow
+
+            // Mix with Blue
+        }
+
+        // First Color Blue
+        else if (PaintBucket_1.GetComponent<PaintCan>().ColorName == "BLUE")
+        {
+            // Mix with Yellow
+
+            // Mix with Blue
+        }
+
+        
+        return Color.black;
+    }
+
+}
