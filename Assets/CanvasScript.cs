476e5f8431835e6aac51ba9b1e000b7d5ef9c526
M4A1CID <konata_lzumi@hotmail.com>
2016-08-30 13:44:24 +0800

    Added paint by numbers

---
+++ Assets/CanvasScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanvasScript : MonoBehaviour {
+
+    private ComputeBitmap computeBitmap=new ComputeBitmap();
+    public Texture2D PaintByNumTexture;
+    public Texture2D MainTexture;
+    
+	// Use this for initialization
+	void Start () {
+        // Slap the outline onto the existing canvas
+        MainTexture = computeBitmap.BitmapsAddMix(MainTexture, PaintByNumTexture, 0.5f, 0.5f);
+        this.transform.GetComponent<Renderer>().material.mainTexture = MainTexture as Texture;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
